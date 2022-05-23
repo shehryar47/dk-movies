e30493d8d16138b39c2047774f6ca48494beda6e
Shehryar <shehryarhassan9@gmail.com>
2022-05-23 23:13:34 +0500

    Added Modal In Html

--- src/app/indian-movies/indian-movies.component.ts
+++ src/app/indian-movies/indian-movies.component.ts
@@ -12,6 +12,7 @@ export class IndianMoviesComponent implements OnInit {
       id: 1,
       name: 'Gol Maal',
       year: 2003,
+      img:"https://images.unsplash.com/photo-1653290188582-76e0571d332a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
       quality: 'HD',
       trailerLink: 'https://www.youtube.com/watch?v=A1-PEkuuWi8'
     },
@@ -20,28 +21,33 @@ export class IndianMoviesComponent implements OnInit {
       name: 'Chakor Maal',
       year: 2004,
       quality: 'HD',
-      trailerLink: ' https://www.youtube.com/watch?v=A1-PEkuuWi8'
+      img:"https://images.unsplash.com/photo-1653290188582-76e0571d332a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+
+      trailerLink: 'https://www.youtube.com/watch?v=A1-PEkuuWi8'
     },
     {
       id: 3,
       name: 'Lamba Maal',
       year: 2005,
       quality: 'HD',
-      trailerLink: ' https://www.youtube.com/watch?v=A1-PEkuuWi8'
+      img:"https://images.unsplash.com/photo-1653290188582-76e0571d332a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+      trailerLink: 'https://www.youtube.com/watch?v=A1-PEkuuWi8'
     },
     {
       id: 4,
       name: 'Chota Maal',
       year: 2006,
       quality: 'HD',
-      trailerLink: ' https://www.youtube.com/watch?v=A1-PEkuuWi8'
+      img:"https://images.unsplash.com/photo-1653290188582-76e0571d332a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+      trailerLink: 'https://www.youtube.com/watch?v=A1-PEkuuWi8'
     },
     {
       id: 5,
       name: 'Mota Maal',
       year: 2007,
       quality: 'HD',
-      trailerLink: ' https://www.youtube.com/watch?v=A1-PEkuuWi8'
+      img:"https://images.unsplash.com/photo-1653290188582-76e0571d332a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+      trailerLink: 'https://www.youtube.com/watch?v=A1-PEkuuWi8'
     },
   ]
 
