9cc5178efa133bbceeefc7336be2a0cb3c58b57b
Shehryar <shehryarhassan9@gmail.com>
2022-05-25 14:49:06 +0500

    Fixed Trailer links

--- src/app/indian-movies/indian-movies.component.ts
+++ src/app/indian-movies/indian-movies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-indian-movies',
   templateUrl: './indian-movies.component.html',
@@ -14,7 +15,7 @@ export class IndianMoviesComponent implements OnInit {
       year: 2003,
       img:"https://images.unsplash.com/photo-1653290188582-76e0571d332a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
       quality: 'HD',
-      trailerLink: 'https://www.youtube.com/watch?v=A1-PEkuuWi8'
+      trailer: "https://www.youtube.com/embed/qqpyWWlLevo"
     },
     {
       id: 2,
@@ -23,7 +24,7 @@ export class IndianMoviesComponent implements OnInit {
       quality: 'HD',
       img:"https://images.unsplash.com/photo-1653290188582-76e0571d332a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
 
-      trailerLink: 'https://www.youtube.com/watch?v=A1-PEkuuWi8'
+      trailer: "https://www.youtube.com/embed/qqpyWWlLevo"
     },
     {
       id: 3,
@@ -31,7 +32,7 @@ export class IndianMoviesComponent implements OnInit {
       year: 2005,
       quality: 'HD',
       img:"https://images.unsplash.com/photo-1653290188582-76e0571d332a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
-      trailerLink: 'https://www.youtube.com/watch?v=A1-PEkuuWi8'
+      trailer: 'https://www.youtube.com/embed/qqpyWWlLevo'
     },
     {
       id: 4,
@@ -39,7 +40,7 @@ export class IndianMoviesComponent implements OnInit {
       year: 2006,
       quality: 'HD',
       img:"https://images.unsplash.com/photo-1653290188582-76e0571d332a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
-      trailerLink: 'https://www.youtube.com/watch?v=A1-PEkuuWi8'
+      trailer: 'https://www.youtube.com/embed/qqpyWWlLevo'
     },
     {
       id: 5,
@@ -47,10 +48,14 @@ export class IndianMoviesComponent implements OnInit {
       year: 2007,
       quality: 'HD',
       img:"https://images.unsplash.com/photo-1653290188582-76e0571d332a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
-      trailerLink: 'https://www.youtube.com/watch?v=A1-PEkuuWi8'
+      trailer: 'https://www.youtube.com/embed/qqpyWWlLevo'
     },
+
   ]
 
+
+ 
+  
   constructor() { }
 
   ngOnInit(): void {
